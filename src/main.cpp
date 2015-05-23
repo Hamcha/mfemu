843a4a9ab99dd85d83c39ccd8ff89a49c9aba092
silverweed <silverweed1991@gmail.com>
2015-05-23 12:50:12 +0200

    added optional file argument to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,22 @@
 #include "CPU.h"
 #define DEBUG 1
 
-int main() {
+int main(int argc, char **argv) {
 	std::cout << "mfemu v." << VERSION << " rev." << COMMIT << std::endl << std::endl;
-	ROM file = ROM::FromFile("test.gb");
+	std::string romFile = "test.gb";
+	for (int i = 1; i < argc; ++i) {
+		if (argv[i][0] == '-') {
+			switch (argv[i][1]) {
+			case 'v':
+				return 0; // we already printed the version
+			default:
+				std::cout << "Usage: " << argv[0] << " [-h] [file.gb]" << std::endl;
+				return 0;
+			}
+		}
+		romFile = std::string(argv[i]);
+	}
+	ROM file = ROM::FromFile(romFile);
 
 	// The title can be either 15 or 13 characters, depending on target console
 	std::string title;
