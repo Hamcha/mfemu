49aac9e193c6f64f5ef24300d223f6ce158ca419
Hamcha <zikyky@gmail.com>
2015-06-29 15:00:07 +0200

    Some incomplete drawing logic

--- src/GPU.cpp
+++ src/GPU.cpp
@@ -1,6 +1,6 @@
 #include "GPU.h"
 
-const uint8_t shades[] = { 0xff, 0xc0, 0x60, 0x00 };
+const uint32_t shades[] = { 0xffffffff, 0xffc0c0c0, 0xff606060, 0xff000000 };
 
 void GPU::Step(const int cycles) {
 	if (!lcdControl.flags.enableLCD) {
@@ -80,13 +80,40 @@ void GPU::InitScreen(SDL_Renderer* _renderer) {
 }
 
 void GPU::drawLine() {
-	uint16_t mapOffset = lcdControl.flags.bgTileTable ? 0x1c00 : 0x1800;
+	if (lcdControl.flags.displayBackground) {
+		uint16_t mapOffset = lcdControl.flags.bgTileTable ? 0x1c00 : 0x1800;
+		uint16_t dataOffset = lcdControl.flags.tilePatternTable ? 0x0800 : 0x0000;
 
-	//TODO Calculate offsets
-	for (int tx = bgScrollX; tx <= bgScrollX + WIDTH; tx += 8) {
-		//TODO Palette
+		uint8_t lineOffset = line % 8;
 
-		//TODO Plot pixel
+		//TODO Calculate scroll offset
+
+		for (int tx = 0; tx <= WIDTH / 8; tx += 1) {
+			// Get tile id
+			uint8_t tileId = VRAM[VRAMbankId].bytes[mapOffset + tx];
+
+			// Convert signed to unsigned if Pattern table #1
+			if (lcdControl.flags.tilePatternTable == 1) {
+				tileId ^= 0x80;
+			}
+
+			// Get colors (2 bytes) of the current tile's line
+			uint16_t colorOffset = dataOffset + tileId * 16 + lineOffset * 2;
+			uint8_t color0 = VRAM[VRAMbankId].bytes[colorOffset];
+			uint8_t color1 = VRAM[VRAMbankId].bytes[colorOffset+1];
+
+			//TODO Plot line
+			for (int pixel = 0; pixel < 8; pixel += 1) {
+				// This is a 2 bit number, MSB is color0[pixel], LSB is color1[pixel]
+				uint8_t colorId = (color0 >> pixel & 0x1 << 1) | (color1 >> pixel & 0x1);
+
+				// Get actual color from the palette
+				uint8_t actualColor = (bgPalette.raw >> (colorId * 2)) & 0x3;
+
+				// Set pixel to shade defined by the color
+				screen[line * WIDTH + tx * 8 + pixel] = shades[actualColor];
+			}
+		}
 	}
 }
 
